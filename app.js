3ae68e93c1f12958800e508caa4fdf0a3a2ef741
vitaliy_shlepakov <shlepakov58@gmail.com>
2019-04-01 20:57:55 +0300

    finish weather app

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+
+//Init UI
+const ui = new UI();
+// //Init storage
+const storage = new Storage();
+//Get storage locationData
+const weatherLocation = storage.getLocationData();
 // Init weather
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 //Get weather onload
 document.addEventListener('DOMContentLoaded', getWeather);
 
-weather.changeLocation('Miami', 'Fl');
+//Change LocATION
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+
+    storage.setLocationData(city, state)
+
+    //Get and display weather
+    weather.getWeather()
+
+    //Close modal
+    $('#locModal').modal('hide');
+});
+
+
 
 function getWeather(){
     weather.getWeather()
         .then(results => {
-            console.log(results)
+            ui.paint(results)
         })
         .catch(err => console.log(err));
 }
